a0cd1113793bb7e302e9dc7e7919c946abdd6531
Dan Johnson <dan@designmynight.com>
2018-02-16 17:09:38 +0000

    fix: gulp now builds CSS and JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,30 @@
-var requireDir = require('require-dir');
+var gulp = require('gulp')
+    sass = require('gulp-sass')
+    minifycss = require('gulp-minify-css')
+    plumber = require('gulp-plumber')
+    livereload = require('gulp-livereload');
 
-// Require all tasks in gulp, including subfolders
-requireDir('./gulp', { recurse: true });
+var packagesToInclude = [
+    './node_modules/highlight.js/lib/highlight.js'
+];
+
+gulp.task('css', function() {
+    return gulp.src('src/styles/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(minifycss({advanced:false}))
+        .pipe(gulp.dest('dist/stylesheets'))
+        .pipe(livereload());
+});
+
+gulp.task('js', function() {
+    return gulp.src(packagesToInclude)
+        .pipe(gulp.dest('./dist/javascripts/'));
+});
+
+gulp.task('css-watch', function() {
+    livereload.listen();
+    gulp.watch('src/styles/*.scss', ['css'])
+});
+
+gulp.task('default', ['js', 'css'])
